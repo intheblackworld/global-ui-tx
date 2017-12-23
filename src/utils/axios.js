cc53bb09ee34f70da0836fa566812e46cbf848ae
David <david@globalsoftbest.com>
2017-12-23 11:43:05 +0800

    变更 jsop 串接网址机制

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import qs from 'qs'
 import fetchJsonp from 'fetch-jsonp'
+import { map } from 'ramda'
 
 
 export const axiosService = (params) => {
@@ -13,20 +14,16 @@ export const axiosService = (params) => {
 }
 
 export const jsonpService = (parameters) => {
-  const {
-    url,
-    ticketId, // 31 //分分彩  36 //3D彩
-    limit, // 近100期
-    
-    // 分分彩 0,1,2,3,4,99: 万, 千, 百, 十, 个, 总和
-    // 3D彩 0,1,2,99: 百, 十, 个, 总和
-    locations,
-    
-    // 分分彩 10,11,47 大小, 单双, 龙虎
-    // 3D彩 10, 11 大小, 单双
-    types
-  } = parameters
-  return fetchJsonp(`http://trend.ybf01.com/trends/trend/${url}?ticketId=${ticketId}&limit=${limit}&locations=${locations}&types=${types}`, {
+  const { url } = parameters
+  const paramList = Object.keys(parameters)
+
+  const params = map((param) => {
+    if (param === 'url') {
+      return
+    }
+    return `${param}=${parameters[param]}`
+  })(paramList).join('&')
+  return fetchJsonp(`http://trend.ybf01.com/trends/trend/${url}?${params}`, {
     jsonp: 'callback'
   })
 }
